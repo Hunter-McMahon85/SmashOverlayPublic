fa8bf0d1bf122635cd29d792cea313ca82149a06
Hunter-McMahon85 <hmcmahon@uoregon.edu>
2024-02-19 00:23:02 -0800

    router added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css'
+import Home from './pages/home';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/instructions" element={<Home />} />
+        <Route path="/sessionconfig" element={<Home />} />
+        <Route path="/scorebug" element={<Home />} />
+        <Route path="/controller" element={<Home />} />
+        <Route path="*" element={<Home />} />
+      </Routes>
+    </Router>
   )
 }
 
---
+++ src/pages/home.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+function Home() {
+
+  return (
+    <>
+      <div className="home_contain">
+        <p>Hello World</p>
+      </div>
+    </>
+  )
+}
+
+export default Home
